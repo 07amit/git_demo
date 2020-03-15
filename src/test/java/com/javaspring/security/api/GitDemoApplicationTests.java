7329e75db39677545cf76cf7c2bca80726ce52dd
amit <amit07.himt@gmail.com>
2020-03-15 11:19:34 +0530

    second commit

--- src/test/java/com/javaspring/security/api/GitDemoApplicationTests.java
+++ src/test/java/com/javaspring/security/api/GitDemoApplicationTests.java
@@ -1,10 +1,14 @@
 package com.javaspring.security.api;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GitDemoApplicationTests {
+	
+	Logger logger=LoggerFactory.getLogger(GitDemoApplicationTests.class);
 
 	@Test
 	void contextLoads() {
